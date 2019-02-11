ba7092e1855fbf2d51612b3dcaf7af484156eca8
Sofiia Materynska <materynskasofiyka@gmail.com>
2019-02-11 21:30:38 +0200

    Borders and randomize function to setter are added(instead of Controller class)

--- Task2/src/Model.java
+++ Task2/src/Model.java
@@ -8,11 +8,32 @@ public class Model {
 
     private int intNumber;
 
+    private int topBorderOfRange=100;
+
+    private int bottomBorderOfRange=0;
+
+
     public int getIntNumber() {
         return intNumber;
     }
 
-    public void setIntNumber(int intNumber) {
-        this.intNumber = intNumber;
+    public void setIntNumber() {
+        this.intNumber = (int)(Math.random()*(getTopBorderOfRange()-getBottomBorderOfRange()+1))+getBottomBorderOfRange();
+    }
+
+    public int getBottomBorderOfRange() {
+        return bottomBorderOfRange;
+    }
+
+    public void setBottomBorderOfRange(int bottomBorderOfRange) {
+        this.bottomBorderOfRange = bottomBorderOfRange;
+    }
+
+    public int getTopBorderOfRange() {
+        return topBorderOfRange;
+    }
+
+    public void setTopBorderOfRange(int topBorderOfRange) {
+        this.topBorderOfRange = topBorderOfRange;
     }
 }
